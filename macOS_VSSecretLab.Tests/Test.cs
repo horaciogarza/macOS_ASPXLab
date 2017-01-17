d4c0f9f7f5974441e47ade8ca1ac3051aa0f0760
Horacio Garza <hhgarza95@gmail.com>
2017-01-16 23:03:01 -0600

    Initial Commit

---
+++ macOS_VSSecretLab.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace macOS_VSSecretLab.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void TestCase()
+		{
+		}
+	}
+}
---
+++ macOS_VSSecretLab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="macOS_VSSecretLab.Global" %>
